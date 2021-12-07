a39591d2f9bd49a0eb56744746e8f60d071d0e20
Lucas Tepper <lucas.tepper.91@gmail.com>
2021-12-07 11:27:24 +0100

    corr 1d+2d_broadcast

--- correlation/correlation.py
+++ correlation/correlation.py
@@ -3,42 +3,44 @@ import mkl_fft
 
 
 def mkl_res_conj_inplace(x):
-    """ The resulting array of the intel_mkl.fft inplace function
-    (overwrite_x=True) writes the results in the form
-    Real(Nyquist freq), half the spectrum, Real(zero freq).
+    """ The resulting array of the intel_mkl.fft, broadcasting along
+    the first dimension, writes the results in the form Axis 0: n_ffts
+    Axis1: Real(Nyquist freq), half the spectrum, Real(zero freq).
     This is possible due to the hermetian spectrum of real data.
     To complex conjugate this we want to conj all but the
     first and last element.
     Arguments:
-        real valued np.ndarray, output of mkl.rfft
+        real valued 2d np.ndarray, output of mkl.rfft
     Returns:
         pointer to input array """
 
-    x[1:-1] = np.conj(x[1:-1].view(np.complex128)).view(np.float64)
+    for i in range(len(x)):
+        x[i, 1:-1] = np.conj(x[i, 1:-1].view(np.complex128)).view(np.float64)
     return x
 
 
 def mkl_res_mult_inplace(x, y):
-    """ The resulting array of the intel_mkl.fft inplace function
-    (overwrite_x=True) writes the results in the form
-    Real(Nyquist freq), half the spectrum, Real(zero freq).
+    """ The resulting array of the intel_mkl.fft, broadcasting along
+    the first dimension, writes the results in the form Axis 0: n_ffts
+    Axis1: Real(Nyquist freq), half the spectrum, Real(zero freq).
     This is possible due to the hermetian spectrum of real data.
     To multiply two arrays, we need to do complex multiplication
     for spectrum and pointwise for first and last elem.
     Arguments:
-        (x) real valued np.ndarray, output of mkl.rfft
-        (y) real valued np.ndarray, output of mkl.rfft
+        (x) real valued 2d np.ndarray, output of mkl.rfft
+        (y) real valued 2d np.ndarray, output of mkl.rfft
     Returns:
         (x) real valued np.ndarray, output of mkl.rfft,
             written in input x """
 
-    x[1:-1] = (x[1:-1].view(np.complex128) * y[1:-1].view(np.complex128)).view(np.float64)
-    x[0] *= y[0]
-    x[-1] *= y[-1]
+    for i in range(len(x)):
+        x[i, 1:-1] = (x[i, 1:-1].view(np.complex128) * y[i, 1:-1].view(np.complex128)).view(np.float64)
+    x[:, 0] *= y[:, 0]
+    x[:, -1] *= y[:, -1]
     return x
 
 
-def correlation(a, b, trunc=None, overwrite=False):
+def correlation(a_input, b_input, trunc=None):
     """ Calculates correlation via FFT. Only tested for 1d data.
     Zero pads the data. Zero padding twice makes no difference,
     so we do not check if it has been done already. We check
@@ -49,33 +51,38 @@ def correlation(a, b, trunc=None, overwrite=False):
         a (np.ndarray, real): input 1
         b (np.ndarray, real): input 2
         trunc (int): number of out elements to keep
-        overwrite (bool): Overwrite input
     Returns (np.ndarray): correlation """
 
-    assert isinstance(a, np.ndarray)
-    assert isinstance(b, np.ndarray)
-    assert a.ndim == 1
-    assert a.shape == b.shape
-    assert np.isreal(a.all())
-    assert np.isreal(b.all())
+    assert isinstance(a_input, np.ndarray)
+    assert isinstance(b_input, np.ndarray)
+    assert a_input.shape == b_input.shape
+    assert np.isreal(a_input.all())
+    assert np.isreal(b_input.all())
     assert isinstance(trunc, (int, type(None)))
 
-    if not trunc:
-        trunc = len(a)
-    trunc = min(trunc, len(a))
-    len_a = len(a)
-
-    a = np.append(a, np.zeros(len_a))
-    a = mkl_fft.rfft(a, n=None, axis=-1, overwrite_x=overwrite)
-    if b is None:
-        a1 = mkl_res_conj_inplace(np.copy(a))
-        a = mkl_res_mult_inplace(a, a1)
-        del a1
+    # The code assumes 2d shape, reshape from 1d, maintain input var for later
+    if a_input.ndim == 1:
+        a, b = a_input.reshape(1, -1), b_input.reshape(1, -1)
     else:
-        b = np.append(b, np.zeros(len_a))
-        b = mkl_fft.rfft(b, n=None, axis=-1, overwrite_x=overwrite)
+        a, b = a_input, b_input
+    # store shape of data for later
+    ndim, len_a = a.shape
+    if not trunc:
+        trunc = len_a
+    trunc = min(trunc, len_a)
+
+    a = np.ascontiguousarray(np.concatenate([a, np.zeros_like(a)], axis=1))
+    a = mkl_fft.rfft(a, n=None, axis=-1, overwrite_x=True).reshape(ndim, -1)
+    if id(a_input) == id(b_input):
+        a1 = mkl_res_conj_inplace(np.copy(a))
+        a = mkl_res_mult_inplace(a1, a)
+    else:
+        b = np.ascontiguousarray(np.concatenate([b, np.zeros_like(b)], axis=1))
+        b = mkl_fft.rfft(b, n=None, axis=-1, overwrite_x=True).reshape(ndim, -1)
         a = mkl_res_conj_inplace(a)
         a = mkl_res_mult_inplace(a, b)
-    a = mkl_fft.irfft(a, n=None, axis=-1, overwrite_x=overwrite)[:trunc]
-    a /= np.linspace(len_a, len_a - trunc + 1, trunc)
+    a = mkl_fft.irfft(a, n=None, axis=-1, overwrite_x=True).reshape(ndim, -1)[:, :trunc]
+    a /= np.linspace(len_a, len_a - trunc + 1, trunc).reshape(1, -1)
+    if len(a) == 1:
+        return a.flatten()
     return a
--- test/test_correlation.py
+++ test/test_correlation.py
@@ -1,3 +1,5 @@
+import os
+os.chdir('..')
 import numpy as np
 from correlation import correlation
 
@@ -20,7 +22,7 @@ def test_autocorr_1d():
     data = np.sin(time) + np.random.normal(scale=0.1, size=size)
     data_copy = np.copy(data)
     np.testing.assert_allclose(correlation(data, data), correlation_ref(data, data, size))
-    # somehow, setting overwrite does not input corrupt data
+    # somehow, setting overwrite in correlation does not corrupt input data
     np.testing.assert_allclose(data, data_copy)
 
 
@@ -33,5 +35,34 @@ def test_corr_1d():
     data2 = np.sin(time) + np.random.normal(scale=0.1, size=size)
     data2_copy = np.copy(data2)
     np.testing.assert_allclose(correlation(data1, data2), correlation_ref(data1, data2, size))
+    np.testing.assert_allclose(data1, data1_copy)
+    np.testing.assert_allclose(data2, data2_copy)
+
+
+def test_autocorr_2d():
+    size = (3, int(1e3 + 11))
+    np.random.seed(42)
+    time = np.arange(size[1]) * 0.02
+    data = np.sin(time) + np.random.normal(scale=0.1, size=size)
+    data_copy = np.copy(data)
+    ref = np.zeros_like(data)
+    for i, dat in enumerate(data):
+        ref[i] = correlation_ref(dat, dat, data.shape[1])
+    np.testing.assert_allclose(correlation(data, data), ref)
+    np.testing.assert_allclose(data, data_copy)
+
+
+def test_corr_2d():
+    size = (3, int(1e3 + 11))
+    np.random.seed(42)
+    time = np.arange(size[1]) * 0.02
+    data1 = np.sin(time) + np.random.normal(scale=0.1, size=size)
+    data1_copy = np.copy(data1)
+    data2 = np.sin(time) + np.random.normal(scale=0.1, size=size)
+    data2_copy = np.copy(data2)
+    ref = np.zeros_like(data1)
+    for i, (dat1, dat2) in enumerate(zip(data1, data2)):
+        ref[i] = correlation_ref(dat1, dat2, data1.shape[1])
+    np.testing.assert_allclose(correlation(data1, data2), ref)
     np.testing.assert_allclose(data1, data1_copy)
     np.testing.assert_allclose(data2, data2_copy)
